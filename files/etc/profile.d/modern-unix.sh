dc2b2c147da4c702409f213e7fcedd23ac90dd80
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2024-05-04 20:54:27 +0800

    grab stuff from bluefin-cli

---
+++ files/etc/profile.d/modern-unix.sh
@@ -0,0 +1,12 @@
+# shellcheck shell=sh
+# Eza for ls
+alias ll='eza -l --icons=auto --group-directories-first' 2>/dev/null
+alias l.='eza -d .*' 2>/dev/null
+alias ls='eza' 2>/dev/null
+alias l1='eza -1'
+
+# Zoxide is handling cd
+alias cd='cd' 2>/dev/null
+
+# neovim for vim
+alias vim='nvim'
